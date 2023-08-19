cc5cc6772aab842d816afce5b94321db9cf66cf5
apollostudios159 <apollostudios159@gmail.com>
2023-08-19 17:04:39 +0530

    Adding context

--- src/App.js
+++ src/App.js
@@ -1,134 +1,56 @@
-import "./App.css";
-import PropTypes from "prop-types";
 import React from "react";
 import styled from "@emotion/styled";
-import { Button } from "@mui/material";
+import { CssBaseline } from "@mui/material";
+import PokemonInfo from "./components/PokemonInfo";
+import PokemonFilter from "./components/PokemonFilter";
+import PokemonTable from "./components/PokemonTable";
+import "./App.css";
 
 const Title = styled.h1`
   text-align: center;
 `;
-
+const PageContainer = styled.div`
+  margin: auto;
+  width: 800px;
+  padding-top: 1em;
+`;
 const TwoColumnLayout = styled.div`
   display: grid;
-  grid-template-columns: 70% 30%;
+  grid-template-columns: 80% 20%;
   grid-column-gap: 1rem;
 `;
 
-const Container = styled.div`
-  margin: auto;
-  width: 800px;
-  padding-top: 1rem;
-`;
-
-const Input = styled.input`
-  width: 100%;
-  font-size: x-large;
-  padding: 0.2rem;
-`;
-
-const PokemonRow = ({ pokemon, onSelect }) => (
-  <tr>
-    <td>{pokemon.name.english}</td>
-    <td>{pokemon.type.join(", ")}</td>
-    <td>
-      <Button
-        variant="contained"
-        color="primary"
-        onClick={() => onSelect(pokemon)}
-      >
-        Select
-      </Button>
-    </td>
-  </tr>
-);
-const PokemonInfo = ({ name, base }) => (
-  <div>
-    <h1>{name.english}</h1>
-    <table>
-      <tbody>
-        {Object.keys(base).map((key) => (
-          <tr key={key}>
-            <td>{key}</td>
-            <td>{base[key]}</td>
-          </tr>
-        ))}
-      </tbody>
-    </table>
-  </div>
-);
-
-PokemonRow.propTypes = {
-  pokemon: PropTypes.shape({
-    name: PropTypes.shape({ english: PropTypes.string }),
-    type: PropTypes.arrayOf(PropTypes.string),
-    onSelect: PropTypes.func,
-  }),
-};
-
-PokemonInfo.propTypes = {
-  name: PropTypes.shape({
-    english: PropTypes.string,
-  }),
-  base: PropTypes.shape({
-    HP: PropTypes.number.isRequired,
-    Attack: PropTypes.number.isRequired,
-    Defense: PropTypes.number.isRequired,
-    "Sp. Attack": PropTypes.number.isRequired,
-    "Sp. Defense": PropTypes.number.isRequired,
-    Speed: PropTypes.number.isRequired,
-  }),
-};
-
 function App() {
-  const [filter, setFilter] = React.useState("");
-  const [selectedItem, setSelectedItem] = React.useState(null);
-  const [pokemon, setPokemon] = React.useState([]);
+  const [filter, filterSet] = React.useState("");
+  const [pokemon, pokemonSet] = React.useState(null);
+  const [selectedPokemon, selectedPokemonSet] = React.useState(null);
 
   React.useEffect(() => {
-    fetch(`https://apollostudios159.github.io/starting-react/pokemon.json`)
+    fetch("/starting-react/pokemon.json")
       .then((resp) => resp.json())
-      .then((data) => setPokemon(data));
+      .then((data) => pokemonSet(data));
   }, []);
 
+  if (!pokemon) {
+    return <div>Loading data</div>;
+  }
+
   return (
-    <Container>
+    <PageContainer>
+      <CssBaseline />
       <Title>Pokemon Search</Title>
-      <Input
-        value={filter}
-        onChange={(event) => {
-          setFilter(event.target.value);
-        }}
-      />
       <TwoColumnLayout>
         <div>
-          <table width="100%">
-            <thead>
-              <tr>
-                <th>Name</th>
-                <th>Type</th>
-              </tr>
-            </thead>
-            <tbody>
-              {pokemon
-                .filter((pokemon) =>
-                  pokemon.name.english
-                    .toLowerCase()
-                    .includes(filter.toLowerCase())
-                )
-                .slice(0, 20)
-                .map((pokemon) => (
-                  <PokemonRow
-                    pokemon={pokemon}
-                    key={pokemon.id}
-                    onSelect={(pokemon) => setSelectedItem(pokemon)}
-                  ></PokemonRow>
-                ))}
-            </tbody>
-          </table>
+          <PokemonFilter filter={filter} filterSet={filterSet}></PokemonFilter>
+          <PokemonTable
+            pokemon={pokemon}
+            selectedPokemonSet={selectedPokemonSet}
+            filter={filter}
+          ></PokemonTable>
         </div>
-        {selectedItem && <PokemonInfo {...selectedItem}></PokemonInfo>}
+        {selectedPokemon && <PokemonInfo {...selectedPokemon} />}
       </TwoColumnLayout>
-    </Container>
+    </PageContainer>
   );
 }
 
